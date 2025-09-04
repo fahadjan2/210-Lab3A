02d06b4b66ed09fe6e7b7ea6812a27a3fa9fd5f3
fahadjan2 <fahadfawad2006@gmail.com>
2025-09-03 19:14:00 -0700

    committing, there was an error in my vs code that half of my time to fix

--- code.cpp
+++ code.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+
 struct Restaurant{
     int hourOpen;
     int hourClose;
@@ -10,6 +11,48 @@ struct Restaurant{
     int rating;
 };
 
+Restaurant initializer();
+void printData(Restaurant);
+
 int main() {
-   cout << "e" << endl;
+    Restaurant restaurantInputted = initializer();  
+    printData(restaurantInputted);
+}
+
+
+Restaurant initializer(){
+    Restaurant restaurantInput;
+    cout << "Restaurant Info:" << endl;
+    cout << "Enter hour restaurant opens(24hr)" << endl;
+    cin >> restaurantInput.hourOpen;
+    cout << "Enter hour restaurant closes(24hr)" << endl;
+    cin >> restaurantInput.hourClose;
+
+    while (restaurantInput.hourOpen >= 24 or restaurantInput.hourOpen < 0 or restaurantInput.hourClose >= 24 or restaurantInput.hourClose < 0){
+        cout << "Hours entered wrong, must be within 0-23" << endl;
+        cin >> restaurantInput.rating;
+    } 
+
+    cout << "Enter type of restaurant cuisine" << endl;
+    cin >> restaurantInput.type;
+
+    cout << "Enter restaurant address" << endl;
+    cin >> restaurantInput.address;
+
+    cout << "Enter restaurant rating(1-5)" << endl;
+    cin >> restaurantInput.rating;
+    while (restaurantInput.rating > 5 or restaurantInput.rating < 1){
+        cout << "Value must be within 1-5" << endl;
+        cin >> restaurantInput.rating;
+    }
+
+    return restaurantInput;
+}
+
+
+void printData(Restaurant restaurantData){
+    cout << "Restaurant opens at " << restaurantData.hourOpen << " and closes at " << restaurantData.hourClose << endl;
+    cout << "Restaurant is a " << restaurantData.type << " type of restaurant" << endl;
+    cout << "Restaurant address is " << restaurantData.address << endl;
+    cout << "Restaurant rating is " << restaurantData.rating << " stars" << endl;
 }
